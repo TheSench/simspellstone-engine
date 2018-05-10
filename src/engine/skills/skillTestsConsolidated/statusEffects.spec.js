704b4251a8733d7b189045592bcfe03820ee8324
Jon Senchyna <TheSench@users.noreply.github.com>
2018-05-10 07:00:33 -0400

    Fixed statsu effects based on status values

--- src/engine/skills/skillTestsConsolidated/statusEffects.spec.js
+++ src/engine/skills/skillTestsConsolidated/statusEffects.spec.js
@@ -8,18 +8,23 @@ export const applicationTypes = {
   replace: 'applicationType.replace'
 };
 
-export function shouldApplyStatusTo({executeSkill, target}, affectedStatus, applicationType) {
+export function shouldApplyStatusTo({executeSkill, target}, affectedStatus, applicationType, applicationValueSource) {
   describe(`effect on ${target}.status.${affectedStatus}`, () => {
-    let targetUnit,
+    let targetUnit = null,
       skillInstance;
 
     beforeEach(() => {
       targetUnit = createTestUnit();
-      skillInstance = { value: 5 };
+      if (applicationValueSource) {
+        targetUnit.status[applicationValueSource] = 5;
+      }
+      skillInstance = { value: (applicationValueSource ? -1 : 5) };
     });
 
     switch (applicationType) {
       case applicationTypes.max:
+        basicStatusApplication(executeSkill, affectedStatus, { sourceStatus: applicationValueSource });
+
         it(`should replace lower values of ${affectedStatus}`, () => {
           targetUnit.status[affectedStatus] = 3;
 
@@ -37,7 +42,9 @@ export function shouldApplyStatusTo({executeSkill, target}, affectedStatus, appl
         });
         break;
       case applicationTypes.stack:
-        it(`should stack with previous ${affectedStatus}`, () => {
+        basicStatusApplication(executeSkill, affectedStatus, { sourceStatus: applicationValueSource });
+
+        it(`should stack with the current value of ${affectedStatus}`, () => {
           targetUnit.status[affectedStatus] = 3;
 
           executeSkill(skillInstance, targetUnit);
@@ -46,20 +53,72 @@ export function shouldApplyStatusTo({executeSkill, target}, affectedStatus, appl
         });
         break;
       default:
-        [1, 99].forEach(function replaceStatus(flatValue) {
-          it(`should always set value of ${affectedStatus} to skill value`, () => {
-            let expectedValue = (applicationType !== applicationTypes.replace ? applicationType : skillInstance.value);
-
-            targetUnit.status[affectedStatus] = flatValue;
+        if (/[+-]\d+/.test(applicationType)) {
+          let value = parseInt(applicationType);
+          let change = (value > 0 ? 'increment' : 'decrement');
+          it(`should ${change} the current value of ${affectedStatus} by ${value}`, () => {
+            let startingValue = 1 + Math.abs(value);
+            let expectedValue = startingValue + value;
+            targetUnit.status[affectedStatus] = startingValue;
 
             executeSkill(skillInstance, targetUnit);
 
             expect(targetUnit.status[affectedStatus], affectedStatus).to.equal(expectedValue);
           });
-        });
+        } else {
+          [1, 99].forEach(function replaceStatus(flatValue) {
+            let valueSource = (applicationValueSource ? `${applicationValueSource} status` : 'effect')
+            it(`should always set value of ${affectedStatus} to ${valueSource} value`, () => {
+              let expectedValue = (applicationType !== applicationTypes.replace ? applicationType : skillInstance.value);
+
+              targetUnit.status[affectedStatus] = flatValue;
+
+              executeSkill(skillInstance, targetUnit);
+
+              expect(targetUnit.status[affectedStatus], affectedStatus).to.equal(expectedValue);
+            });
+          });
+        }
         break;
     }
   });
+}
+
+function basicStatusApplication(executeSkill, affectedStatus, { sourceStatus, flatValue } = {}) {
+  [1, 2, 99].forEach((testValue) => {
+    let expectedValue = (sourceStatus !== undefined
+      ? sourceStatus
+      : flatValue !== undefined
+        ? flatValue
+        : testValue);
+
+    it(`given a value of ${testValue}, it should apply a status of ${affectedStatus} equal to ${expectedValue}`, () => {
+      let unit = createTestUnit();
+      if (sourceStatus) {
+        unit.status[sourceStatus] = 5;
+        expectedValue = 5;
+      }
+
+      let effectInstance = { id: 'effectInTest', value: testValue };
+
+      executeSkill(effectInstance, unit);
+
+      expect(unit.status[affectedStatus], affectedStatus).to.equal(expectedValue);
+    });
+  });
+
+  if (sourceStatus) {
+    [1, 2, 99].forEach((testValue) => {
+      it(`given a ${sourceStatus} status of ${testValue}, it should apply a status of ${affectedStatus} equal to ${testValue}`, () => {
+        let unit = createTestUnit({ status: { [sourceStatus]: testValue } });
+        let effectInstance = { id: 'effectInTest', value: 5 };
+
+        executeSkill(effectInstance, unit);
+
+        expect(unit.status[affectedStatus], affectedStatus).to.equal(testValue);
+      });
+    });
+  }
 }
 
 export function shouldAffectNoOtherStatuses({executeSkill, affectedStatuses, target}) {
